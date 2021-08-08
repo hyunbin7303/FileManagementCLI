e996c229f892caa0462dbdfd6c5be4849f10a6f8
Zio4 <64350954+Zio4@users.noreply.github.com>
2021-08-08 20:46:32 +0900

    revision1
    1. Personcode가 유니크한지 체크
    2. Method로 문법체크(숫자,알파벳)
    3. Contiue로 문법틀릴시 바로 다음라인으로 진행

--- 20210706_check/Program.cs
+++ 20210706_check/Program.cs
@@ -47,91 +47,162 @@ namespace ConsoleApp4
         int check=0;
         int check_grammer=0;
         int alphabet=0;
+        int Digit=0;
 
         while((line = Person_1.ReadLine()) != null)  
         {   
-            check++;
+            if(check==4)
+            {   
+                check_grammer=0;
+                check=0;
+                check++;
+            }
+            else
+            {
+                check++;
+            }
+        
             Lindex= line.IndexOf("---------");
             if (Lindex==0)
             {
                 check--;
                 continue;
             }
-            Lindex= line.LastIndexOf("Height:");
-            if(Lindex!=100 && Lindex!=-1)
-            {   
-                textreader=line.Substring(line.IndexOf("Height:") + "Height:".Length);
-                try//int로 변환되면 등록 아니면 스킵
-                {   aa.Height=(Convert.ToInt16(textreader));
-                    check_grammer++;
-                }
-                catch(FormatException)
-                    {   
-                //        Console.WriteLine("Height가 int형이 아닙니다.");
-                    }                
-            }
+
             Lindex= line.LastIndexOf("PersonCode:");
             if(Lindex!=100 && Lindex!=-1)
-            {
+            {   Console.WriteLine("{0}",line);
                 textreader=line.Substring(line.IndexOf("PersonCode:") + "PersonCode:".Length);
-                try//int로 변환되면 등록 아니면 스킵
-                {   aa.PersonCode=(Convert.ToInt16(textreader));
-                    check_grammer++;
+                foreach(char ch in textreader)
+                {   
+                    Digit=0;
+                    if(IsNumeric(ch) == true)//숫자인지 검사하는거
+                    {
+                        Digit++;
+                    }
+                    else
+                    {
+                        break;
+                    }
                 }
-                catch(FormatException)
-                    {   
-                      //  Console.WriteLine("Personcode가 int형이 아닙니다.");
-                    }    
+                if(Digit==1)
+                {   
+                    foreach(Person p in people)
+                    {
+                        if(p.PersonCode==Convert.ToInt16(textreader))//Person Code에 대한 중복확인.
+                        {
+                            Digit=0;
+                            break;
+                        }
+                    }                    
+                    if(Digit == 1 )
+                    {
+                        aa.PersonCode=(Convert.ToInt16(textreader));
+                        check_grammer++;
+                        Digit=0;
+                    }
+                    else
+                    {
+                        continue;
+                    }
+                }
+                else if(Digit==0)
+                {    
+                    continue;
+                }
             }
+    
             Lindex= line.LastIndexOf("FirstName:");
             if(Lindex!=100 && Lindex!=-1)
             {
                 textreader=line.Substring(line.IndexOf("FirstName:") + "FirstName:".Length);
-                foreach(char ch in textreader)//알파벳인지 검사하는거
-                {
-                    if ((0x61 > ch || ch > 0x7A) && (0x41 > ch || ch > 0x5A))
+                foreach(char ch in textreader)
+                {   
+                    alphabet=0;
+                    if(IsEnglish(ch) == true)//알파벳인지 검사하는거
                     {
                         alphabet++;
                     }
+                    else
+                    {
+                        break;
+                    }
                 }
-                if(alphabet==0)
+                if(alphabet==1)
                 {
                     aa.First=(textreader);
                     check_grammer++;
-                }
-                else if(alphabet>0)
-                {    
-                  //  Console.WriteLine("Firstname이 str형이 아닙니다.");
                     alphabet=0;
                 }
+                else if(alphabet==0)
+                {    
+                  //  Console.WriteLine("Firstname이 str형이 아닙니다.");
+                    continue;
+                }
             }
+
             Lindex= line.LastIndexOf("LastName:");
             if(Lindex!=100 && Lindex!=-1)
             {
                 textreader=line.Substring(line.IndexOf("LastName:") + "LastName:".Length);
-                foreach(char ch in textreader)//알파벳인지 검사하는 거
-                {
-                    if ((0x61 > ch || ch > 0x7A) && (0x41 > ch || ch > 0x5A))
+                foreach(char ch in textreader)
+                {   
+                    alphabet=0;
+                    if(IsEnglish(ch) == true)//알파벳인지 검사하는거
                     {
                         alphabet++;
                     }
+                    else
+                    {
+                        break;
+                    }
                 }
-                if(alphabet==0)
+                if(alphabet==1)
                 {
                     aa.Last=(textreader);
                     check_grammer++;
-                }
-                else if(alphabet>0)
-                {    
-                 //   Console.WriteLine("Lastname이 str형이 아닙니다.");
                     alphabet=0;
                 }
-            }           
+                else if(alphabet==0)
+                {    
+                  //  Console.WriteLine("Lasttname이 str형이 아닙니다.");
+                    continue;
+                }
+            }
+
+            Lindex= line.LastIndexOf("Height:");
+            if(Lindex!=100 && Lindex!=-1)
+            {
+                textreader=line.Substring(line.IndexOf("Height:") + "Height:".Length);
+                foreach(char ch in textreader)
+                {   
+                    Digit=0;
+                    if(IsNumeric(ch) == true)//숫자인지 검사하는거
+                    {
+                        Digit++;
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+                if(Digit==1)
+                {
+                    aa.Height=(Convert.ToInt16(textreader));
+                    check_grammer++;
+                    Digit=0;                    
+                }
+                else if(Digit==0)
+                {    
+                    continue;
+                }
+            }
+
             if(check==4)
             {
                 if(check_grammer==4)
                 {
-                    // Console.WriteLine("{0}",aa.PersonCode);
+                    // Console.WriteLine("기록{0}",aa.PersonCode);
                     // Console.WriteLine("{0}",aa.Height);
                     // Console.WriteLine("{0}",aa.First);
                     // Console.WriteLine("{0}",aa.Last);
@@ -146,12 +217,28 @@ namespace ConsoleApp4
                     check=0;
                     check_grammer=0;
                 }
-             }                                                 
+            }
+            else
+            Console.WriteLine("check확인{0}",check);                                                 
         }  
         foreach (Person p in people)
              Console.WriteLine("personcode: {0}, FirstName : {1}, LastName : {2}, Height {3}",p.PersonCode, p.First, p.Last, p.Height);
             // Console.WriteLine("personcode: {0}",p.PersonCode);
 
         }
-    }
+        static bool IsEnglish(char ch)//알파벳인지 검사하는거
+        {   
+            if ((0x61 <= ch && ch <= 0x7A) || (0x41 <= ch && ch <= 0x5A))
+                return true;
+            else
+                return false;
+        }
+        static bool IsNumeric(char ch)//숫자인지 검사하는거
+        {
+            if (0x30 <= ch && ch <= 0x39)
+                return true;
+            else
+                return false;
+        }
+    }        
 }
