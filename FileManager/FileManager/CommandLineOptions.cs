4ae5682e638faac2fdee8526db26f3a12e857c20
kevin <hyunbin7303@gmail.com>
2021-09-03 09:58:08 -0400

    Init update

---
+++ FileManager/FileManager/CommandLineOptions.cs
@@ -0,0 +1,21 @@
+﻿using CommandLine;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileManager
+{
+    public class CommandLineOptions
+    {
+        [Value(index: 0, Required = true, HelpText = "Image file Path to analyze.")]
+        public string Path { get; set; }
+
+        [Option(shortName: 'c', longName: "confidence", Required = false, HelpText = "Minimum confidence.", Default = 0.9f)]
+        public float Confidence { get; set; }
+
+        [Option('v', "verbose", Required = false, HelpText = "Set output to verbose messages.")]
+        public bool Verbose { get; set; }
+    }
+}
---
+++ FileManager/FileManager/Program.cs
@@ -0,0 +1,64 @@
+﻿using CommandLine;
+using FileManager.Command;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.IO;
+using System.Reflection;
+using System.Threading.Tasks;
+
+namespace FileManager
+{
+    public class Options
+    {
+        [Option('m', "max", Required =false,Default =5000, HelpText ="The maximum number : ")]
+        public int MaxRandomInt { get; set; }
+
+        [Value(index: 0, Required = true, HelpText = "The file to display information for.")]
+        public string Path { get; set; }
+
+        [Option(shortName: 'c', longName: "confidence", Required = false, HelpText = "Minimum confidence.", Default = 0.9f)]
+        public float Confidence { get; set; }
+
+        [Option('d', "detailed", HelpText = "Whether to output detailed information about the file.")]
+        public bool Detailed { get; set; }
+    }
+    public interface IConfig
+    {
+        string FileUploaderDestination { get; set; }
+    }
+    public class Config : IConfig
+    {
+        public string FileUploaderDestination { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+    }
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+         //   IServiceProvider serviceProvider = BuildServiceProvider(args);
+
+            Parser.Default.ParseArguments<Options>(args)
+                   .WithParsed(o =>
+                   {
+                   })
+                   .WithNotParsed(e =>
+                   {
+                       Console.WriteLine("ERROR");
+                   });
+        }
+
+
+        static IServiceProvider BuildServiceProvider(string[] args)
+        {
+            IServiceCollection collection = new ServiceCollection();
+            IConfiguration configuration = new ConfigurationBuilder().AddJsonFile("appSettings.json", optional: false).AddCommandLine(args).Build();
+            IConfig config = configuration.Get<Config>();
+
+            collection.AddSingleton<Options>();
+            collection.AddSingleton<IConfig>(config);
+            return collection.BuildServiceProvider();
+        }
+
+    }
+}
